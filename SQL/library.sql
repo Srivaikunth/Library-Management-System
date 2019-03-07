bfe735b4be6c8a8b49a22621d40dca133896af08
sriv2122 <sriv2122@CSLH1711.global.chainsys.com>
2019-03-07 16:23:34 +0530

    commit-sql

--- SQL/library.sql
+++ SQL/library.sql
@@ -1,29 +1,31 @@
 Create table student(
 student_id number(10),
-student_name varchar2(20),
-phone_number number(10),
-address varchar2(30)
-);
+student_name varchar2(40) not null,
+phone_number number(10) not null,
+address varchar2(30) not null,
+password varchar2(20) not null);
+
+
 create sequence student_student_id_seq
 start with 1
 increment by 1;
-alter table student add password varchar2(20);
-select * from student;
-desc student;
+
+
+
 Create table author(
 author_id number(10),
-author_name varchar2(20), constraint pk_author_id primary key(author_id)
+author_name varchar2(40) not null, constraint pk_author_id primary key(author_id)
 );
 
 create sequence author_author_id_seq
 start with 1
 increment by 1;
 
-select* from user_cons_columns where TABLE_NAME='AUTHOR';
+
 
 Create table publication(
 id number(10),
-name varchar2(20),constraint pk_id primary key(id)
+name varchar2(30) not null,constraint pk_id primary key(id)
 );
 
 create sequence publication_id_seq
@@ -34,14 +36,16 @@ increment by 1;
 create table book_details(
 id number(10),
 isbn number(20) not null,
-name varchar2(20) not null,
-price number(10) not null,
+name varchar2(40) not null,
+price float(10) not null,
 publication_id number(10),
 author_id number(10), constraint pk_book_id primary key(id),
+constraint uk_isbn unique (isbn),
 constraint fk_publication_id foreign key (publication_id) references publication(id),
 constraint fk_author_id foreign key(author_id) references author (author_id)
 );
-select * from book_details;
+
+
 create sequence id_seq
 start with 1
 increment by 1;
@@ -55,27 +59,18 @@ insert into admin values('vaikunth','siddharth');
 
 select * from author;
 select * from book_details;
-delete  from  book_details;
-insert into book_details values(1,12345,'java',120,2,1,0);
-insert into book_details values(3,87654,'Sql',156,2,1,0);
-commit;
-
-insert into author(author_id,author_name) values(1,'janani');
-insert into publication(id,name) values(2,'oxford');
-
-select * from author;
-
-update book_details set price= where;
-select * from  author;
-delete from publication;
-delete from author;
-
-
 select * from student;
+
 
 alter table book_details add status int;
 update book_details set status=1;
 
 commit;
 
-alter table book_details modify(unique(isbn));
+
+
+
+drop table book_details;
+drop table author;
+drop table publication;
+drop table student;
